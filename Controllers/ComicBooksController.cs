147fd2e300e1dbb91567fe04548d3fb64545fee6
Andrew Storrow <andrew.storrow@gmail.com>
2018-02-04 20:53:33 -0600

    Added Series List View

--- Controllers/ComicBooksController.cs
+++ Controllers/ComicBooksController.cs
@@ -14,6 +14,7 @@ namespace ComicBookMVC.Controllers
             _comicBookRepository = new ComicBookRepository();
         }
 
+        // GET: ComicBooks
         public ActionResult Index()
         {
             var comicBooks = _comicBookRepository.GetComicBooks();
@@ -21,6 +22,7 @@ namespace ComicBookMVC.Controllers
             return View(comicBooks);
         }
 
+        // GET: ComicBooks/Detail/Id
         public ActionResult Detail(int? id)
         {
             if(id == null)
---
+++ Controllers/SeriesController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ComicBookMVC.Models;
+using ComicBookMVC.Data;
+
+namespace ComicBookMVC.Controllers
+{
+    public class SeriesController : Controller
+    {
+        private SeriesRepository _seriesRepository = null;
+
+        public SeriesController()
+        {
+            _seriesRepository = new SeriesRepository();
+        }
+
+        // GET: Series
+        public ActionResult Index()
+        {
+            var series = _seriesRepository.GetSeries();
+
+            return View(series);
+        }
+    }
+}
